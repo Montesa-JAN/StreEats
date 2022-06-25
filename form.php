48351ca4a55b3a0923e9080cbc01889cde5f1680
Montesa-JAN <audhy.montesa@gmail.com>
2022-06-25 10:08:22 +0800

    minor changes

--- form.php
+++ form.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                 "Subject: $subject.\n".
                  "User Message: $message .\n";
 
-$to = 'infostreEats@gmail.com'
+$to = 'streEatsinfo@gmail.com'
 
 $headers = "From: $email_from \r\n";
 
